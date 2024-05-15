42402dce37d5a7b40babadc656c5e652f2006090
leticiakalila <leticiakaliladarlene@gmail.com>
2024-05-16 01:03:39 +0700

    navigate to database

--- marmut_merah_jambu/settings.py
+++ marmut_merah_jambu/settings.py
@@ -80,10 +80,16 @@ WSGI_APPLICATION = 'marmut_merah_jambu.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        "NAME": "postgres",
+        "USER": "postgres.cucamuatyldtkvxripor",
+        "PASSWORD": "Basdat-7-marmut",
+        "HOST": "aws-0-ap-southeast-1.pooler.supabase.com",
+        "PORT": "5432",
+        "OPTIONS": {"options": "-c search_path=marmut"},
     }
 }
+
 
 
 # Password validation
